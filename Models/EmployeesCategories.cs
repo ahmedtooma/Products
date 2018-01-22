b77f2894a0fe461710df2c382faf8428cc096d06
ahmedtooma <30286017+ahmedtooma@users.noreply.github.com>
2018-01-22 19:08:49 +0200

    Add files via upload

---
+++ Models/EmployeesCategories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace third.Models
+{
+    public class EmployeesCategories
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
